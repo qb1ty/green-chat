c63a72e896fbfa9d5320b9704eccd0f520ae06be
qb1ty <arsenbro0707@mail.ru>
2026-09-24 22:03:15 +0500

    chore: configure eslint and prettier rules

---
+++ prettier.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import("prettier").Config} */
+export default {
+	trailingComma: "none",
+	tabWidth: 4,
+	useTabs: true,
+	semi: false,
+	singleQuote: false,
+	jsxSingleQuote: false,
+	arrowParens: "avoid",
+	importOrderSeparation: true,
+	importOrderSortSpecifiers: true,
+	importOrderCaseInsensitive: true,
+	importOrderParserPlugins: [
+		"classProperties",
+		"decorators-legacy",
+		"typescript",
+		"jsx"
+	],
+	importOrder: ["<THIRD_PARTY_MODULES>", "^/(.*)$", "^../(.*)", "^./(.*)"],
+	plugins: ["@trivago/prettier-plugin-sort-imports"]
+}
