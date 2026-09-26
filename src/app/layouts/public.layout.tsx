cc91f2ce096d43d514b5433b006fb7a71484e30c
qb1ty <arsenbro0707@mail.ru>
2026-09-26 19:30:40 +0500

    feat: setup public layout with global toasts integration

--- src/app/layouts/public.layout.tsx
+++ src/app/layouts/public.layout.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom"
 
+import { Toasts } from "@/shared/ui/toast"
+
 export default function PublicLayout() {
 	return (
-		<div className="flex-1 bg-(--bg-primary)">
+		<div className="flex-1 min-h-screen  bg-(--bg-primary)">
 			<Outlet />
+			<Toasts />
 		</div>
 	)
 }
