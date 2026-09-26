6f3b92ac300d8b86e253df87b061cb997496b8ad
qb1ty <arsenbro0707@mail.ru>
2026-09-26 19:30:08 +0500

    feat: implement responsive chat UI, split aside components, and add auto-resizing chat input

--- src/app/layouts/private.layout.tsx
+++ src/app/layouts/private.layout.tsx
@@ -1,9 +1,29 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useLocation } from "react-router-dom"
+
+import { Aside } from "@/shared/ui/chats"
+import { Toasts } from "@/shared/ui/toast"
 
 export default function PrivateLayout() {
+	const location = useLocation()
+
+	const isChatSelected =
+		location.pathname !== "/chat" && location.pathname !== "/chat/"
+
 	return (
-		<div>
-			<Outlet />
+		<div className="flex h-screen w-full overflow-hidden bg-(--bg-primary)">
+			<div
+				className={`${isChatSelected ? "hidden sm:block" : "block"} w-full sm:w-1/3 lg:w-1/4`}
+			>
+				<Aside />
+			</div>
+
+			<main
+				className={`${!isChatSelected ? "hidden sm:flex" : "flex"} flex-1 relative min-w-0`}
+			>
+				<Outlet />
+			</main>
+
+			<Toasts />
 		</div>
 	)
 }
